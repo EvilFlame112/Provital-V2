dc80f129f889d22908a420ac9a2da46fd0395385
EvilFlame112 <rampranav25@gmail.com>
2024-12-11 01:09:31 +0530

    lots of edits

--- src/components/carousel/LifestylePillars.tsx
+++ src/components/carousel/LifestylePillars.tsx
@@ -157,6 +157,8 @@ const cards = [
   },
 ];
 
+
+
 const LifestylePillars = () => {
   const [activeIndex, setActiveIndex] = useState(0);
   const cardContainerRef = useRef<HTMLDivElement>(null);
@@ -237,14 +239,18 @@ const LifestylePillars = () => {
                 onClick={() => handleArrowClick("left")}
                 disabled={activeIndex === 0}
               >
-                &#8592;
+                <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M5 12L19 12M5 12L11 18M5 12L11 6" stroke="#747474" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                </svg>
               </button>
               <button
                 className={styles.arrow}
                 onClick={() => handleArrowClick("right")}
                 disabled={activeIndex === cards.length - 1}
               >
-                &#8594;
+                <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M19 12L5 12M19 12L13 18M19 12L13 6" stroke="#747474" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                </svg>
               </button>
             </div>
           </>
@@ -263,7 +269,7 @@ const LifestylePillars = () => {
               }`}
               style={{ width: isMobile ? "282px" : "auto", height: isMobile ? "418px" : "auto" }}
             >
-              <img src={card.image} alt={card.title} style={{ width: "282px", height: isMobile ? "255px" : "320px" }} />
+              <img src={card.image} alt={card.title} style={{ width: isMobile ? "282px" : "493px", height: isMobile ? "255px" : "320px" }} />
               <div className={styles.pressure}>
                 <div className={styles.statimg}>{card.statimg}</div>
                 <div className={styles.stat}>{card.stat}</div>
